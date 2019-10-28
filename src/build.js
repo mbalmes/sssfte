98d98a44c46ba2344ec6e9fbac4979d1bb1f2fa4
unknown <mbalmes12@gmail.com>
2019-10-28 00:04:32 -0600

    support for titles

--- src/build.js
+++ src/build.js
@@ -2,25 +2,54 @@ const mkdirp = require("mkdirp");
 const fs = require("fs");
 const _ = require("lodash");
 
+const showdown = require("showdown");
+const converter = new showdown.Converter();
 const buildFolder = process.argv[process.argv.length - 1];
 
-const buildLinkList = function() {
-  return `<ul>${fs
+const getMarkdownFiles = function() {
+  return fs
     .readdirSync(`${buildFolder}/content`)
-    .filter(x => _.endsWith(x, ".md"))
-    .map(md=>md.replace('.md',''))
-    .map(md => `<li><a href="${md}.html">${md}</a></li>`)
+    .filter(x => _.endsWith(x, ".md"));
+};
+
+const buildLinkList = function() {
+  const markdownFiles = getMarkdownFiles();
+  const markdownFilesWithTitles = _.zip(
+    markdownFiles,
+    markdownFiles.map(f =>
+        getTitleFromMarkdown(fs.readFileSync(`${buildFolder}/content/${f}`,'utf-8'))
+    )
+  );
+  return `<ul>${markdownFilesWithTitles
+    .map(x => `<li><a href="${x[0]}.html">${x[1]}</a></li>`)
     .join("")}
     </ul>`;
 };
 
+const buildDefaultContent = function() {
+  return converter.makeHtml(
+    fs.readFileSync(`${buildFolder}/content/index.md`, "utf-8")
+  );
+};
+
+const getTitleFromMarkdown = function(markdown) {
+    const match = markdown.match(/#\s.*/);
+    return match === null ? "" : match[0].replace(/#\s/,'');
+};
+
 mkdirp(`${buildFolder}/_build`, function(err) {
-  const templateFileContents = fs.readFileSync(
+  let templateFileContents = fs.readFileSync(
     `${buildFolder}/index.html`,
     "utf-8"
   );
-  fs.writeFileSync(
-    `${buildFolder}/_build/index.html`,
-    templateFileContents.replace(/{ links }/i, buildLinkList())
+  templateFileContents = templateFileContents.replace(
+    /{ links }/i,
+    buildLinkList()
   );
+  templateFileContents = templateFileContents.replace(
+    /{ content }/i,
+    buildDefaultContent()
+  );
+
+  fs.writeFileSync(`${buildFolder}/_build/index.html`, templateFileContents);
 });
